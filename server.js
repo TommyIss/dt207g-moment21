73ac18c741d82439961f41069f34a1b7a4154efa
Tommy Issa <tommyissa@hotmail.com>
2025-05-21 13:21:51 +0200

    Har lämnat tillbaka lösenord inställning

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 let connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD || '',
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
 });
 
