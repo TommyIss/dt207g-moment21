8749bcc369aa2bac3e43460e3cd358731c0400e7
Tommy Issa <tommyissa@hotmail.com>
2025-05-21 10:43:34 +0200

    Har lagt ett till alternative för lösenordet

--- install.js
+++ install.js
@@ -6,7 +6,7 @@ let app = express();
 let connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASSWORD || '',
     database: process.env.DB_DATABASE,
 });
 
